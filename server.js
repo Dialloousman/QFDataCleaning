c438de59248897a9f8d29db4acec3ac9b2b61ad6
Ousman <ordiallo@gmail.com>
2020-03-02 11:59:55 -0500

    database write method written and tested

--- server.js
+++ server.js
@@ -102,7 +102,21 @@ async function problem3TrackingRunningTotals() {
     });
   }
 
-  console.log('classificationAndTotals', classificationAndTotals);
+  //   console.log('classificationAndTotals', classificationAndTotals);
+  // write to db
+
+  function writeClassificationAndTotalsToDB(classificationAndTotalsRow) {
+    // create query string to insert new message
+    const queryString = 'INSERT INTO classification_totals (Classification, Totals) VALUES ($1, $2)';
+
+    const valuesToInsert = [classificationAndTotalsRow.classification, classificationAndTotalsRow.total];
+    db.query(queryString, valuesToInsert, (err, result) => {
+    // error handle
+      if (err) return ('Error inserting into DB', err);
+    //   console.log('rows successfully inserted', result);
+    });
+  }
+  classificationAndTotals.forEach(classRow => writeClassificationAndTotalsToDB(row));
   return classificationAndTotals;
 }
 problem3TrackingRunningTotals();
