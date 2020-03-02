c699809e915020274b937c66c7fd9753426f4f53
Ousman <ordiallo@gmail.com>
2020-03-02 09:36:25 -0500

    problemSet1 initalized

---
+++ model/model.js
@@ -0,0 +1,19 @@
+
+// const { Pool } = require('pg');
+
+// const myURI = 'postgres://jiqoikud:Zf9rq6T9_LqglYgGNJnsb_eseI0PaZB7@isilo.db.elephantsql.com:5432/jiqoikud';
+
+// const URI = myURI;
+
+// const pool = new Pool({
+//   connectionString: URI,
+// });
+
+// const db = module.exports = {
+//   query: (text, params, callback) => {
+//     console.log('executed query', text);
+//     return pool.query(text, params, callback);
+//   },
+// }; 
+
+// module.export = 
--- server.js
+++ server.js
@@ -8,43 +8,44 @@ const app = express();
 const PORT = 3434;
 
 
-const unCleanDataSetPath = path.resolve(__dirname, './DataEngineerDataSet.csv');  
+const unCleanDataSetPath = path.resolve(__dirname, './DataEngineerDataSet.csv');
 
-async function retrieveSourceDataFromCSV () {
-    const jsonArrayofSourceData = await csvToJson().fromFile(unCleanDataSetPath);
-    const dataClassificationHashTable = {};
-    const classificationAndTotals = [];
-    
-    function classificationTotalsAndPopulateHashTable(sourceData) {
-        const datasetKeys = Object.keys(sourceData);
-        for (let key of datasetKeys) {
-            if (key === 'Classification') {
-                    if (!dataClassificationHashTable[sourceData[key]]) {
-                        dataClassificationHashTable[sourceData[key]] = 1;
-                    } else dataClassificationHashTable[sourceData[key]]+=1;
-                };
-            };
-        };
-        
-    jsonArrayofSourceData.forEach(rowDataSetfromCsv => {
-        classificationTotalsAndPopulateHashTable(rowDataSetfromCsv);
-    })
+//* **questionSet1Solution
+console.log(csvToJson().fromFile(unCleanDataSetPath));
 
-    //create rows front created hashtable
-    const classificationHashTableKeys = Object.keys(dataClassificationHashTable);
-        for (let classification of classificationHashTableKeys) {
-            classificationAndTotals.push({
-                classification,
-                total: dataClassificationHashTable[classification]
-            })
-        };
-    
-        console.log('classificationAndTotals: ');
-        return classificationAndTotals;
-    };
+//* ** questionSet3Solution
+async function retrieveSourceDataFromCSV() {
+  const jsonArrayofSourceData = await csvToJson().fromFile(unCleanDataSetPath);
+  const dataClassificationHashTable = {};
+  const classificationAndTotals = [];
 
+  function classificationTotalsAndPopulateHashTable(sourceData) {
+    const datasetKeys = Object.keys(sourceData);
+    for (const key of datasetKeys) {
+      if (key === 'Classification') {
+        if (!dataClassificationHashTable[sourceData[key]]) {
+          dataClassificationHashTable[sourceData[key]] = 1;
+        } else dataClassificationHashTable[sourceData[key]] += 1;
+      }
+    }
+  }
 
+  jsonArrayofSourceData.forEach((rowDataSetfromCsv) => {
+    classificationTotalsAndPopulateHashTable(rowDataSetfromCsv);
+  });
 
+  // create rows front created hashtable
+  const classificationHashTableKeys = Object.keys(dataClassificationHashTable);
+  for (const classification of classificationHashTableKeys) {
+    classificationAndTotals.push({
+      classification,
+      total: dataClassificationHashTable[classification],
+    });
+  }
+
+  return classificationAndTotals;
+}
+// retrieveSourceDataFromCSV();
 
 
 // Invoke server listen
